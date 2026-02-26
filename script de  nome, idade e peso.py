4e444306d1dfc4fa26fe15576f0a7893d8249b3b
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-02-25 21:07:25 -0300

    Update script de  nome, idade e peso.py

--- script de  nome, idade e peso.py
+++ script de  nome, idade e peso.py
@@ -1,5 +1,6 @@
 nome = input('qual o seu nome? ')
 idade = input('qual a sua idade? ')
 peso = input('qual é o seu peso? ')
-print('o nome dele(a) é' , nome + ',' , 'a idade dele(a) é' , idade + ' e' , 'o peso dele(a) é' , peso)
+print('o nome dele(a) é:{}, a idade é:{} e o peso é:{}'.format(nome, idade , peso))
       
+
