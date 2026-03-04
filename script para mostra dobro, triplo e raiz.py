57b21178e48db572cfb1ba7ade8a4b1c6e175bb1
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-03-04 20:53:12 -0300

    Update script para mostra dobro, triplo e raiz.py

--- script para mostra dobro, triplo e raiz.py
+++ script para mostra dobro, triplo e raiz.py
@@ -1,5 +1,6 @@
-n = int(input('digite um numero: '))
+n = float(input('digite um numero: '))
 dobro = n * 2
 triplo = n * 3
 raizQ = n ** (1/2)
-print('o dobro do numero {} é: {} \ne o triplo é: {} \ne a raiz é: {} '.format(n, dobro, triplo, raizQ))
+
+print('o dobro do numero {} é: {} \ne o triplo é: {} \ne a raiz é: {:.2f} '.format(n, dobro, triplo, raizQ))
