1b9aac1010f7e0e7855a31077cedc6952718f12c
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-03-04 21:02:33 -0300

    Update script para calcular media.py

--- script para calcular media.py
+++ script para calcular media.py
@@ -2,4 +2,5 @@ nota1 = float(input('digite sua primeira nota '))
 nota2 = float(input('digite sua segunda nota '))
 nota3 = float(input('digite sua terceira nota '))
 media = (nota1 + nota2 + nota3) / 3
-print('sua media final é: {} '.format(media))
+
+print('sua media final é: {:.2f} '.format(media))
