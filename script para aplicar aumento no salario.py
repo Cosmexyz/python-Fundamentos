a11dc018c5483735268e52b8fbe6cec514b21a5a
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-03-05 22:10:59 -0300

    Update script para aplicar aumento no salario.py

--- script para aplicar aumento no salario.py
+++ script para aplicar aumento no salario.py
@@ -1,5 +1,6 @@
 print('<<< Ola, boas noticias! voce teve aumneto de salario, vamos calcular? >>>')
 salario = float(input('digite seu salario: R$'))
+print('-' * 15)
 aumento = (15 * salario) / 100
 salarioF = salario + aumento
-print('salario atual é: R${:.0f} \naumento sera de: R${:.0f} \nsalario final pos aumneto é de: R${:.0f}'.format(salario, aumento, salarioF))
+print('salario atual é: R${:.2f} \naumento será de 15%, que corresponde a: R${:.2f} \nsalario final pós aumento é de: R${:.2f}'.format(salario, aumento, salarioF))
