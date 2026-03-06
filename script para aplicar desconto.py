489939c931ea246d602d282ef5a8afcbec4ed86b
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-03-05 21:41:03 -0300

    Update script para aplicar desconto.py

--- script para aplicar desconto.py
+++ script para aplicar desconto.py
@@ -1,6 +1,8 @@
 print('Ola, escolha o seu produto para receber desconto de 5%')
-preco = float(input('Digite o preço do produto: '))
+preco = float(input('Digite o preço do produto:R$'))
+print('-' * 15)
 desconto = ( 5 * preco) / 100
 precoF = preco - desconto
-print('O valor do produto é: R${} \nO desconto é de: R${:.0f} \nO preço final é de: R${}'.format(preco, desconto, precoF))
+print('O valor do produto é: R${} \nO desconto é de: R${:.0f} \nO preço final é de: R${:.2f}'.format(preco, desconto, precoF))
+
 
