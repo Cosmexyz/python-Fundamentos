af1b02f8bb5ffb6f7456f25cf39def97242e3300
COSME CARVALHO DA SILVA NETO <cosmecarvalhog@gmail.com>
2026-03-05 21:15:10 -0300

    Update script de tabuada.py

--- script de tabuada.py
+++ script de tabuada.py
@@ -1,5 +1,24 @@
 print('<<< BEM VINDO A TABOADA SIMPLES >>>')
-n1 = int(input('digite o primeiro numero: '))
-n2 = int(input('digite o segundo numero: '))
-multi = n1 * n2 
-print('A multiplicação enrtre {} e {} é = {}'.format(n1, n2, multi))
+valorI = int(input('digite para ver sua tabuada '))
+print('---------------')
+multi1 = valorI * 1
+multi2 = valorI * 2
+multi3 = valorI * 3
+multi4 = valorI * 4
+multi5 = valorI * 5
+multi6 = valorI * 6
+multi7 = valorI * 7
+multi8 = valorI * 8
+multi9 = valorI * 9
+multi10 = valorI * 10
+print('{} x 1  = {}'.format(valorI, multi1))
+print('{} x 2  = {}'.format(valorI, multi2))
+print('{} x 3  = {}'.format(valorI, multi3))
+print('{} x 4  = {}'.format(valorI, multi4))
+print('{} x 5  = {}'.format(valorI, multi5))
+print('{} x 6  = {}'.format(valorI, multi6))
+print('{} x 7  = {}'.format(valorI, multi7))
+print('{} x 8  = {}'.format(valorI, multi8))
+print('{} x 9  = {}'.format(valorI, multi9))
+print('{} x 10 = {}'.format(valorI, multi10))
+print('---------------')
