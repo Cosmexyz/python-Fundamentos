e77ebdff0fa309e51693e1c1d65bf1249cb58fd1
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-09 20:14:38 -0300

    Add files via upload

---
+++ import_random.py
@@ -0,0 +1,5 @@
+import random
+n  = random.random()
+n2 = random.randint(1 , 10)
+print('seu numero é {}'.format(n)) #random.random() gera um número aleatório float entre 0 e 1
+print('seu numero é {}'.format(n2)) #random.randidt() gera um número aleatório dentre os argumentos A a B que foram postos dentro do ()
