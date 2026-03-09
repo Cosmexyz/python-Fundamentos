4e0e4eda5aced7e53cbca588a4c72abfa774c000
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-09 20:14:26 -0300

    Add files via upload

---
+++ import_emoji.py
@@ -0,0 +1,4 @@
+import emoji
+print(emoji.emojize('Ola, mundo :thumbs_up:')) 
+# O modulo emoji naõ vem nativo, e para instalar precisa de um terminal, nele digite: pip install [NOME DO MODULO]
+# para desistalar, use pip uninstall [NOME DO MODULO], tambem no terminal
