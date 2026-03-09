40bfb56fe63c406f900cfba31a1e683d5a4488cc
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-09 20:52:18 -0300

    Update real_Para_Inteiro.py

--- real_Para_Inteiro.py
+++ real_Para_Inteiro.py
@@ -1,2 +1,9 @@
-n = float(input('Digite um número Real: '))
-print('O número digitado é {}, que está em sua real. \nEm sua forma inteira fica: {:.0f}.'.format(n, n))
+'''n = float(input('Digite um número: '))
+print('O número digitado é {}, que está em sua real. \nEm sua forma inteira fica: {:.0f}.'.format(n, n))'''
+
+#ou pode ser feito da seguinte maneira#
+
+from math import trunc
+n = float(input('Digite um número: '))
+print('O valor digitado foi: {}, que está em sua forma real. \nEm sua forma inteira fica: {}.'.format(n, trunc(n)))
+
