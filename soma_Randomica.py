84fe81e6a1c34a997e563815a243fe97a25c53ac
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-09 20:15:19 -0300

    Add files via upload

---
+++ soma_Randomica.py
@@ -0,0 +1,7 @@
+import random
+n1 = random.randint(0 , 1000)
+n2 = random.randint(0 , 1000)
+soma = n1 + n2
+print('-' * 28)
+print('O primeiro num random é: {} \nO segundo é: {} \nE a soma entre eles é: {}'.format(n1, n2, soma))
+print('-' * 28)
