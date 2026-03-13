510f0bd295a12609d065b530dba037eff2126b69
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-12 21:32:16 -0300

    Update lista_randomica.py

--- lista_randomica.py
+++ lista_randomica.py
@@ -1,9 +1,9 @@
-import random
+from random import shuffle
 print('<<< Vai sortear uma ordem de lista randomica >>> ')
 aluno1 = input('Primeiro aluno: ')
 aluno2 = input('Segundo aluno: ')
 aluno3 = input('Terceiro aluno: ')
 aluno4 = input('Quarto aluno: ')
 alunos = [aluno1, aluno2, aluno3, aluno4]
-sorteio = random.shuffle(alunos)
+sorteio = shuffle(alunos)
 print('a ordem randomica de apresentação foi:\n{} '.format(alunos))
