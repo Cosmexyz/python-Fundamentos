355335ccdce9aaed36eabe930633835cac3e13e1
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-13 21:10:46 -0300

    Add files via upload

---
+++ tocador_MP3.py
@@ -0,0 +1,3 @@
+from playsound3 import playsound
+playsound(r'C:\Users\User\local_arquivo\arquivo.mp3') 
+#O r minusculo antes da string. Isso instrui o Python a tratar os backslashes como caracteres literais, não como caracteres de escape.
