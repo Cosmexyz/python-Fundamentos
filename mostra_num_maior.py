89bbb408621715a596b8d7c9251de51b6b6856fe
COSME CYRILO DE CARVALHO <cosmecarvalhog@gmail.com>
2026-03-16 20:48:44 -0300

    Add files via upload

---
+++ mostra_num_maior.py
@@ -0,0 +1,12 @@
+num1 = int(input('Digite o PRIMEIRO número: '))
+num2 = int(input('Digite o SEGUNDO número: '))
+num3 = int(input('Digite o TRERCEIRO número: '))
+print('-' * 15)
+if num1 > num2:
+    print('O PRIMEIRO é maior')
+elif num2 > num3:
+    print('O SEGUNDO é amior')
+else:
+    print('O TERCEIRO é maior')
+print('-' * 15)
+
